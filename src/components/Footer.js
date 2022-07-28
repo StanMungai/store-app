7aecaa17f2fed408ba590d742bccea0008f0db47
stanford kangethe <stanfordkangethe16@gmail.com>
2022-07-28 13:09:13 +0300

    Setting up the structure of the project

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <h2>Working Hours</h2>
+      <p>Monday - Saturday (8am to 6pm)</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Partners.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Partners() {
+  return (
+   <section className='partners'>
+    <div>
+      <h2>Simba Cement</h2>
+      <p>The leading cement manufacturing and marketing company in the Eastern Africa region, well reputed for our high quality operations, products and expansive capacity.</p>
+    </div>
+    <div>
+      <h2>Mabati Rolling Mills</h2>
+      <p>An integrated Coated steel facility at Mariakani producing world class Coated and Painted Coated Steel products from which it manufactures a plethora of prestigious building solutions in its facilities across Kenya.</p>
+    </div>
+    <div>
+      <h2>Crown Paints</h2>
+      <p>the most innovative paint company in East Africa, providing tailor-made solutions to the construction and retail segments of the market with innovative products, services and world-class after-sales support.</p>
+    </div>
+   </section> 
+  )
+}
+
+export default Partners
